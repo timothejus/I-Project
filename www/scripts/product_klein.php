280ddadbb4b989b697ae8dc5d179cdc676e87177
Sjoerd Scheffer <sjoerd@b00n.org>
2016-11-28 14:09:13 +0100

    index.php gemaakt met 1 product_klein, product_klein in een functie

--- www/scripts/product_klein.php
+++ www/scripts/product_klein.php
@@ -6,26 +6,28 @@
  * Time: 13:37
  */
 
-$titel = 'Titel';
-$plaatje = '../www/images/box.png';
-$prijs = '6,50';
-$resttijd = '00:39:20';
+function geefProductKlein () {
+	$titel = 'Titel';
+	$plaatje = '../www/images/box.png';
+	$prijs = '6,50';
+	$resttijd = '00:39:20';
 
-?>
-<div class="col-sm-3">
-	<div class="panel panel-default">
-		<div class="panel-heading"><a href="#" class="panelheader-link"><?=$titel ?></a></div>
-		<div class="panel-body text-center">
-			<img src="<?=$plaatje ?>" class="img-thumbnail img-responsive img-thumbnail-overview" alt="img"><br/>
-		</div>
-		<div class="panel-footer">
-			<table class="table table-responsive">
-				<tr>
-					<th class="text-center">&euro;<?=$prijs?></th>
-					<th class="text-danger text-center"><?=$resttijd?></th>
-					<th class="text-center"><a href="#" class="btn btn-xs btn-danger">Bied</a></th>
-				</tr>
-			</table>
+	?>
+	<div class="col-sm-3">
+		<div class="panel panel-default">
+			<div class="panel-heading"><a href="#" class="panelheader-link"><?=$titel ?></a></div>
+			<div class="panel-body text-center">
+				<img src="<?=$plaatje ?>" class="img-thumbnail img-responsive img-thumbnail-overview" alt="img"><br/>
+			</div>
+			<div class="panel-footer">
+				<table class="table table-responsive">
+					<tr>
+						<th class="text-center">&euro;<?=$prijs?></th>
+						<th class="text-danger text-center"><?=$resttijd?></th>
+						<th class="text-center"><a href="#" class="btn btn-xs btn-danger">Bied</a></th>
+					</tr>
+				</table>
+			</div>
 		</div>
 	</div>
-</div>
+<?php } ?>
