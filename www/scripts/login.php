66683c444e4a0bc8779d2bc9249c64b439e16d8a
GIGCEES\Cees <gigceez@gmail.com>
2016-11-29 15:39:30 +0100

    Login

--- www/scripts/login.php
+++ www/scripts/login.php
@@ -13,21 +13,19 @@ function login($login, $wachtwoord){
 	{
 			$achternaam = $row['Achternaam'];
 	}
-	if (!isset($achternaam)){
-		return false;
-	}
-	$sql = "SELECT Gebruikersnaam FROM Gebruiker WHERE Gebruikersnaam=(:login) AND Wachtwoord=(:wachtwoord)";
-	$stmt = $dbh->prepare($sql);
-	$stmt->bindParam(':login', $login, PDO::PARAM_INT);
-	$wachtwoord = hash('sha256',$achternaam.$wachtwoord);
-	$stmt->bindParam(':wachtwoord', $wachtwoord, PDO::PARAM_INT);
-	$stmt->execute();
-	while($row = $stmt->fetch(PDO::FETCH_ASSOC))
-	{
-		if(isset($row)){
-			return true;
-		} else {
-			return false;
+	if (isset($achternaam)) {
+		$sql = "SELECT Gebruikersnaam FROM Gebruiker WHERE Gebruikersnaam=(:login) AND Wachtwoord=(:wachtwoord)";
+		$stmt = $dbh->prepare($sql);
+		$stmt->bindParam(':login', $login, PDO::PARAM_INT);
+		$wachtwoord = hash('sha256', $achternaam . $wachtwoord);
+		$stmt->bindParam(':wachtwoord', $wachtwoord, PDO::PARAM_INT);
+		$stmt->execute();
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			if (isset($row)) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 	}
 }
