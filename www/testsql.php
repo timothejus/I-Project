d47a638570ae17c095d97be2d1b8600e141ef7d1
Sjoerd Scheffer <sjoerd@b00n.org>
2016-11-29 10:08:36 +0100

    sql test toegevoegd

---
+++ www/testsql.php
@@ -0,0 +1,12 @@
+<?php
+$dsn = 'sqlsrv:server=192.168.0.20;Database=EenmaalAndermaal', '', '';
+$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+$user = 'sa';
+$password = 'iproject4';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
