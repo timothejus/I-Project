2a215a1771610b823ce4ce9c19e89736db44d87b
Sjoerd Scheffer <sjoerd@b00n.org>
2016-11-29 09:29:28 +0100

    header en footer gesplitst naar scripts

--- www/index.php
+++ www/index.php
@@ -1,108 +1,24 @@
 <?php
-require ("scripts/product_groot.php");
-require ("scripts/product_klein.php");
+require("scripts/product_groot.php");
+require("scripts/product_klein.php");
+require("scripts/header.php");
 ?>
+<!-- Hoofdpagina container -->
+<div class="container">
 
-<!DOCTYPE html>
-<html lang="nl">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Product van de dag -->
+	<div class="row">
 
-		<link href="../www/css/bootstrap.min.css" rel="stylesheet">
-		<link href="../www/css/bootstrap-select.min.css" rel="stylesheet">
+		<?php geefProductGroot(); ?>
 
-		<!-- Kleine CSS fixes -->
-		<link href="../www/css/fixes.css" rel="stylesheet">
+	</div>
 
-		<title>Home</title>
-	</head>
-	<body>
+	<!-- Overige producten -->
+	<div class="row">
 
-		<!-- Scripts voor extra functionaliteit van Bootstrap -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="../www/js/bootstrap.min.js"></script>
-		<script src="../www/js/bootstrap-select.min.js"></script>
+		<!-- Product 1 -->
+		<?php geefProductKlein(); ?>
 
-		<!-- Navbar containers -->
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-
-				<!-- Navbar logo -->
-				<div class="navbar-header">
-					<img class="navbar-brand" src="../www/images/logo.jpg" alt="EenmaalAndermaal">
-					<button type="button" class="navbar-toggle"  data-toggle="collapse" data-target="#loginNav">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-				</div>
-
-				<!-- Inklappend menu voor kleine schermen -->
-				<div class="collapse navbar-collapse" id="loginNav">
-
-					<!-- Zoekbalk/filters -->
-					<div class="navbar-left">
-						<form class="navbar-form">
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Zoek">
-								<div class="selectmenu">
-									<select class="form-control selectpicker">
-										<option class="hide" value="" disabled selected>Categorieen</option>
-										<option value="cat1">Categorie 1</option>
-										<option value="cat2">Categorie 2</option>
-										<option value="cat3">Categorie 3</option>
-										<option value="cat4">Categorie 4</option>
-										<option value="cat5">Categorie 5</option>
-										<option value="cat6">Categorie 6</option>
-										<option value="cat7">Categorie 7</option>
-										<option value="cat8">Categorie 8</option>
-									</select>
-								</div>
-								<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-							</div>
-						</form>
-					</div>
-
-					<!-- Login/registreer/help knoppen -->
-					<div class="navbar-right">
-						<ul class="nav navbar-nav">
-							<li><a href="#">Log in</a></li>
-							<li><a href="#">Registreer</a></li>
-							<li><a href="#">Help</a></li>
-						</ul>
-					</div>
-
-				</div>
-
-			</div>
-		</nav>
-		
-		<!-- Hoofdpagina container -->
-		<div class="container">
-
-			<!-- Product van de dag -->
-			<div class="row">
-
-				<?php geefProductGroot (); ?>
-
-			</div>
-
-			<!-- Overige producten -->
-			<div class="row">
-
-				<!-- Product 1 -->
-				<?php geefProductKlein (); ?>
-
-			</div>
-		</div>
-
-		<div class="container">
-			<footer class="footer text-right">
-				<span class="text-muted">&copy; 2016 Ubera</span>
-			</footer>
-		</div>
-
-	</body>
-</html>
+	</div>
+</div>
+<?php require ("scripts/footer.php");
---
+++ www/scripts/footer.php
@@ -0,0 +1,9 @@
+
+<div class="container">
+	<footer class="footer text-right">
+		<span class="text-muted">&copy; 2016 Ubera</span>
+	</footer>
+</div>
+
+</body>
+</html>
