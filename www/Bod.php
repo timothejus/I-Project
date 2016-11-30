7ac376467efd40a2d25fa8d54908aac90fd2617b
Jip Brouwer <jip_brouwer@hotmail.com>
2016-11-30 13:44:32 +0100

    voorwerp class toegevoegd

--- www/Bod.php
+++ www/Bod.php
@@ -35,7 +35,22 @@ class Bod
 
 	//functies
 	public function plaatsBod(){
+		try {
+			$dbh = getConnection();
+			$sql = "SELECT * FROM Voorwerp";
 
+			$stmt = $dbh->prepare($sql);
+			$stmt->execute();
+
+			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				$voorwerp = new Voorwerp($row["Voorwerpnummer"], $row["Titel"], $row["Beschrijving"], $row["Startprijs"], $row["Betalingswijze"], $row["Plaatsnaam"], $row["Land"], $row["Looptijd"], $row["LooptijdBeginDagTijdstip"], $row["VerzendKosten"], $row["VerzendInstructies"], $row["LooptijdEindeDagTijdstip"], $row["VeilingGesloten"], $row["VerkoopPrijs"]);
+				$voorwerpen[] = $voorwerp;
+
+
+			}
+		} catch (PDOException $e) {
+			echo 'Connection failed: ' . $e->getMessage();
+		}
 	}
 }
 
--- www/index.php
+++ www/index.php
@@ -10,7 +10,7 @@
 require ("scripts/product_groot.php");
 require ("scripts/product_klein.php");
 require ("scripts/format_time.php");
-require ("voorwerp.php");
+require  ("DB.php");
 
 // Maak een connectie met de database.
 $dsn = "sqlsrv:Server=192.168.0.20;Database=EenmaalAndermaal";
@@ -39,6 +39,7 @@ require ("scripts/header.php");
 	<div class="row">
 
 		<?php
+
 		// SQL query voor alle info behalve de aflooptijd. Alle producten waar op geboden is worden weergegeven.
 		$sql = "SELECT * FROM Voorwerp INNER JOIN Bod ON Voorwerp.Voorwerpnummer=Bod.Voorwerp WHERE Bod.Bodbedrag = (SELECT MAX (Bodbedrag) FROM Bod WHERE Bod.Voorwerp=Voorwerp.Voorwerpnummer)";
 
@@ -54,8 +55,15 @@ require ("scripts/header.php");
 			// Tijd wordt geparsed door format_time
 			$tijd = format_time ($tijd);
 
-			$voorwerp = new Voorwerp (1,"d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d");
-			echo $voorwerp -> geefProductKlein ("images/box.png",10,$tijd);
+
+			$voorwerpen = getVoorwerpen();
+
+			foreach ($voorwerpen as $voorwerp) {
+				echo $voorwerp->geefProductKlein("images/box.png",10,$tijd);
+			}
+
+			//$voorwerp = new Voorwerp (1,"d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d");
+			//echo $voorwerp -> geefProductKlein ("images/box.png",10,$tijd);
 
 			// Product wordt weergegeven met de juiste informatie (image moet nog in de database geimplementeerd worden!)
 			//geefProductKlein ($row ['Titel'], "images/box.png", $row ['Bodbedrag'], $tijd);
