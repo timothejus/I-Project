9f90fd6b09fbe0508fd92094738f1a2b9667ba66
Sjoerd Scheffer <sjoerd@b00n.org>
2017-01-09 11:14:58 +0100

    bekijk biedingen html/css

---
+++ www/bekijkbiedingen.php
@@ -0,0 +1,73 @@
+<?php require ("scripts/header.php"); ?>
+
+		<div class="container">
+
+			<!--inloggen-->
+			<div class="row">
+
+				<div class="col-sm-10 col-sm-offset-1">
+					<div class="panel panel-info">
+						<div class="panel-heading text-center"><h4>Biedingen</h4></div>
+						<div class="panel-body">
+							<table class="table mijnbiedingen">
+								<tr>
+									<th>Productnummer</th>
+									<th>Eigen bod</th>
+									<th>Hoogste bod</th>
+									<th>Bied minimaal bod</th>
+								</tr>
+								<tr>
+									<td><a href="#" class="catlink">192048903089</a></td>
+									<td>&euro;100,-</td>
+									<td>&euro;120,-</td>
+									<td><a href="#" class="btn btn-danger btn-xs">Bied &euro;125,-</a></td>
+								</tr>
+								<tr>
+									<td><a href="#" class="catlink">192048903089</a></td>
+									<td>&euro;100,-</td>
+									<td>&euro;120,-</td>
+									<td><a href="#" class="btn btn-danger btn-xs">Bied &euro;125,-</a></td>
+								</tr>
+								<tr>
+									<td><a href="#" class="catlink">192048903089</a></td>
+									<td>&euro;100,-</td>
+									<td>&euro;100,-</td>
+									<td><a href="#" class="btn btn-danger btn-xs disabled">Bied</a></td>
+								</tr>
+								<tr>
+									<td><a href="#" class="catlink">192048903089</a></td>
+									<td>&euro;100,-</td>
+									<td>&euro;120,-</td>
+									<td><a href="#" class="btn btn-danger btn-xs">Bied &euro;125,-</a></td>
+								</tr>
+								<tr>
+									<td><a href="#" class="catlink">192048903089</a></td>
+									<td>&euro;100,-</td>
+									<td>&euro;120,-</td>
+									<td><a href="#" class="btn btn-danger btn-xs">Bied &euro;125,-</a></td>
+								</tr>
+							</table>
+							<div class="text-center">
+								<ul class="pagination">
+									<li class="active"><a>1</a></li>
+									<li><a href="#">2</a></li>
+									<li><a href="#">3</a></li>
+									<li><a href="#">4</a></li>
+									<li><a href="#">5</a></li>
+									<li><a href="#">6</a></li>
+									<li><a href="#">7</a></li>
+									<li><a href="#">8</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="panel-footer text-center">
+							<a href="#" class="btn btn-default">Terug</a>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+
+<?php require ("scripts/footer.php"); ?>
+
